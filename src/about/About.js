03ee7abf61eb59c37c0dff41de32f677112204ff
Steven <stluong123@gmail.com>
2021-05-13 18:21:10 -0700

    updated site to be less text heavy

--- src/about/About.js
+++ src/about/About.js
@@ -6,6 +6,10 @@ import Chloe from 'url:./img/Chloe.png';
 import Restaurant from "url:./img/restaurant.png"
 import Pho from "url:./img/pho.png"
 import Boba from "url:./img/boba.png"
+import StoryIntro from "url:./img/story-intro.png"
+import StoryHistory from "url:./img/story-history.png"
+import StoryGrowth from "url:./img/story-growth.png"
+import StoryCurrent from "url:./img/story-current.png"
 import './About.css';
 import CREATORS_DATA from "./creators-info.json";
 import Creator from './Creator';
@@ -42,23 +46,48 @@ export default class About extends Component {
     return (
       <section className="aboutText">
           <h2 className="centerText">About Our Project</h2>
-          <p>
-            Wander: A Guide to Exploring Seattle is a capstone project from the University of Washington developed to
-            use the power of food to tell the story around Seattle’s Chinatown-International District. Food holds entire
-            legacies that transcend languages and oceans, bringing comfort from one family table to another. Seattle and
-            the CID community is ever-changing, but generations of families have established themselves in the CID and
-            continue to share their story with every delicious bite. Others are at the beginning of their journey, offering
-            something new and building a relationship with the city through food. Our goal for this project is to promote
-            Seattle’s Chinatown-International District and amplify the voices of local businesses, giving them a chance to
-            tell their stories about their food.
-            <br />
-            <br />
-            As land prices rise and gentrification becomes more prominent in areas around Seattle, locals are more likely
-            to be priced out of the communities that have supported them for many years. We hope that users of this guide
-            will be inspired to make a conscious effort to learn more about the history of the Chinatown-International
-            District in an effort to preserve and protect our community. Explore now to learn more about your favorite
-            businesses in the area or discover a new restaurant to fall in love with.
-          </p>
+          <div className="container extraBottomPadding">
+            <div className="row">
+              <div className="col">
+                <h3 className="centerText">Challenges</h3>
+                  <ul>
+                    <li>Tourism can bring economic benefits to Chinatowns but can also amplify gentrification.</li>
+                    <li>Gentrification can potentially alter the culture of the area which leads to a loss of familiarity to the locals.</li>
+                  </ul>
+              </div>
+              <div className="col">
+                <h3 className="centerText">Outcome</h3>
+                <ul>
+                  <li>
+                    Wander is a website that is developed to amplify the stories of food businesses that reside in the Chinatown-International District.
+                  </li>
+                  <li>
+                    Business owners have a platform to tell stories behind their favorite dishes
+                  </li>
+                  <li>
+                    Users can learn about the communities that make up the Chinatown-International District.
+                  </li>
+                </ul>
+              </div>
+              <div className="col">
+              <h3 className="centerText">Impact</h3>
+                <p className="impactStarter">
+                  We hope our project inspires others to...
+                </p>
+                <ul>
+                  <li>
+                  make a conscious effort to respectfully engage with the business
+                  </li>
+                  <li>
+                  learn more about the history of the Chinatown-International District
+                  </li>
+                  <li>
+                  preserve and protect this community
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </section>
     )
   }
@@ -67,38 +96,37 @@ export default class About extends Component {
     return (
       <section>
         <h2 className="centerText">Key Features</h2>
-        <div className="container extraBottomPadding">
+        <div className="container">
           <div className="row">
-            <div className="col-3">
-              <img src = {Restaurant}/>
+            <div className="col extraBottomPadding">
+              <img className="imageShadow" src={StoryIntro}/>
             </div>
             <div className="col align-self-center">
-              <p className="featureText">
-                Disover local favorite restaurants in the Chinatown-International district.
-                The Chinatown-International district is a diverse area that offers various dishes from all across Asia.
-              </p>
+              <h3>Engage with the dishes recommended by the owners themselves</h3>
             </div>
           </div>
           <div className="row">
-            <div className="col-3">
-               <img src = {Boba}/>
+            <div className="col extraBottomPadding">
+              <img className="imageShadow" src={StoryHistory}/>
             </div>
             <div className="col align-self-center">
-             <p className="featureText">
-               Connect with the remakable stories of each businesses.
-               Learn through our interviews with the owners of how their businesses started and grew.
-            </p>
+              <h3>Learn about the origins of your favorite restaurants</h3>
             </div>
           </div>
           <div className="row">
-            <div className="col-3">
-              <img src = {Pho}/>
+            <div className="col extraBottomPadding">
+              <img className="imageShadow" src={StoryGrowth}/>
             </div>
             <div className="col align-self-center">
-              <p className="featureText">
-                Eat amazing food from hidden gems. As you learn more about each businesses, get recommendations of what you should try directly from the owners themselves.
-                These recommendations not only informs you of what to order, but also highlights the greater history behind the dish.
-              </p>
+              <h3>Connect with our interviews to see how the restaurant grew into what it is today</h3>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col align-self-center">
+              <img className="imageShadow" src={StoryCurrent}/>
+            </div>
+            <div className="col align-self-center">
+              <h3>Stay up to date with how the businesses adapted to the pandmemic and their future plans</h3>
             </div>
           </div>
         </div>
@@ -111,7 +139,7 @@ export default class About extends Component {
     return (
       <section>
           <h2 className="centerText">Presentation Video</h2>
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/bQQI7UkbGs4" title="YouTube video player"
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/gW6HwbPqcyc" title="YouTube video player"
             frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen allow="fullscreen">
           </iframe>
           <hr className="dashed"/>
@@ -134,17 +162,20 @@ export default class About extends Component {
     return (
       <section className = "aboutText">
           <h2 className="centerText">Project Status</h2>
-          <p>
-            This project is developed for the University of Washington Information School Capstone for Winter/Spring of 2021. The project is currently being
-            open sourced so that anyone who wishes to develop on this project further can do so. Developers can find our project code <a href="https://github.com/stluongXD/Wander-A-Guide-to-Seattle" target = "_blank">here</a>.
-            <br/>
-            <br/>
-            Documentation as to how to use our files are <a href = "https://docs.google.com/document/d/1aCO4Ft1RI6Dss-IHPh3ZnHBF8wAblZy-ODEvG5aslS0/edit?usp=sharing" target = "_blank">here</a>.
-            Docuementation includes how to set up a new Wordpress site with our files, using Wordpress to create a new story page, and information about the businesses we interviewed from.
-            <br/>
-            <br/>
-            If you are in need of any additional information related to this project, feel free to email anyone from our team and we will get back to you.
-          </p>
+          <ul className="centerContent extraBottomPadding">
+            <li>
+              This project is developed for the University of Washington Information School Capstone for Winter/Spring of 2021
+            </li>
+            <li>
+              This project is currently being open sourced and can be found <a href="https://github.com/stluongXD/Wander-A-Guide-to-Seattle" target = "_blank">here</a>
+            </li>
+            <li>
+              Documentation as to how to use our code are <a href = "https://docs.google.com/document/d/1aCO4Ft1RI6Dss-IHPh3ZnHBF8wAblZy-ODEvG5aslS0/edit?usp=sharing" target = "_blank">here</a>
+            </li>
+            <li>
+              If you are in need of any additional information related to this project, feel free to email anyone from our team and we will get back to you
+            </li>
+          </ul>
         </section>
     )
   }
